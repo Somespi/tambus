ca20e2de590a1e456904d086e51cc54f7084da1e
Someone <120342697+Somespi@users.noreply.github.com>
2023-07-29 20:00:52 +0400

    test: create enginetests.py and test_engine.py

---
+++ tambus/tests/test_engine.py
@@ -0,0 +1,31 @@
+import unittest
+from enginetests import TambusEngine
+
+class TranslateTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.engine = TambusEngine()
+
+    def test_translate_with_variables(self):
+        content = "Hello, {name}!"
+        kwargs = {"name": "John"}
+        expected_result = "Hello, John!"
+        result = self.engine.translate(content, kwargs)
+        self.assertEqual(result, expected_result)
+
+    def test_translate_expressions(self):
+        content = "The result is {result[0]}"
+        kwargs = {"result": [42]}
+        expected_result = "The result is 42"
+        result = self.engine.translate(content, kwargs)
+        self.assertEqual(result, expected_result)
+
+    def test_translate_if(self):
+        content = "{#if condition} True {/if}"
+        kwargs = {"condition": True}
+        expected_result = "True"
+        result = self.engine.translate(content, kwargs)
+        self.assertEqual(result, expected_result)
+
+if __name__ == '__main__':
+    unittest.main()
