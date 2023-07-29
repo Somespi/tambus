4ce0e4d58973a407e6a8c899e34818c62b522ec8
Someone <120342697+Somespi@users.noreply.github.com>
2023-07-29 23:12:29 +0400

    build(deps): update setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tambus",
+    version="0.0.1",
+    author="Someone.",
+    author_email="someonegithub@gmail.com",
+    description="Bambus Template Engine",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/somespi/tambus",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
