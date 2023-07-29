da8301bd1ff24059210329ab52eded6fe5f98225
Someone <120342697+Somespi@users.noreply.github.com>
2023-07-29 14:22:48 +0400

    update engine.cc

--- tambus/src/engine.cc
+++ tambus/src/engine.cc
@@ -23,6 +23,17 @@ private:
     std::map<std::string, std::string> variables;
 
     std::string translate_variables(std::string code) {
-        
+        std::string result = code;
+        std::smatch match;
+        while (std::regex_search(result, match, std::regex("\\{\\{(.*?)\\}\\}"))) {
+            std::string placeholder = match[0].str();
+            std::string variable = match[1].str();
+            if (variables.find(variable) != variables.end()) {
+                result = std::regex_replace(result, std::regex(placeholder), variables[variable]);
+            } else {
+                result = std::regex_replace(result, std::regex(placeholder), "UNKNOWN");
+            }
+        }
+        return result;
     }
 };
