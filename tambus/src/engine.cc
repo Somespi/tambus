c9128eca26c4890a8698459a36ee1ff80afa3e26
Someone <120342697+Somespi@users.noreply.github.com>
2023-07-29 14:08:51 +0400

    update engine.cc

---
+++ tambus/src/engine.cc
@@ -0,0 +1,28 @@
+#include "engine.hpp"
+
+class TambusEngine {
+public:
+    void addVariable(std::string name, std::string value) {
+        variables[name] = value;
+    }
+
+    std::string getVariable(std::string name) {
+        return variables[name];
+    }
+
+    std::string translate( std::string code) {
+        if (std::regex_search(code, std::regex() ) ) {
+            code = translate_variables(code);
+        }
+
+        return code;
+    }
+
+
+private:
+    std::map<std::string, std::string> variables;
+
+    std::string translate_variables(std::string code) {
+        
+    }
+};
