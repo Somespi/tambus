47e063d99601b195168e2341cb31421ab6eb3ddf
Someone <120342697+Somespi@users.noreply.github.com>
2023-07-29 15:37:55 +0400

    modified:   tambus/src/engine.py

--- tambus/src/engine.py
+++ tambus/src/engine.py
@@ -1,29 +1,26 @@
 import re
 
-
 class TambusEngine:
 
-    def translate(self,content: str, **variables):
-        self.varaibles: dict = variables
-        self.content: str = content 
-        
-        if re.search(r"{[^#/:](.*)}", self.content):
+    def translate(self, content: str, **kwargs):
+        self.variables: dict = kwargs
+        self.content: str = content
+
+        if re.search(r"{(.*?)}", self.content):
             self.translate_variables()
-        
-        
+            
         return self.content
 
-
     def translate_variables(self):
-            match = re.search(r"{[^#/:](.*)}", self.content)
-            while match:
-                variable = match.group[1:-2]
-                if variable in list(self.variables.keys()):
-                    self.content = self.content.replace(match.group(), self.variables[variable])
-                match = re.search(r"{[^#/:](.*)}", self.content)
+        match = re.search(r"{(.*?)}", self.content)
+        while match:
+            variable = match.group(1)
+            if variable in self.variables:
+                self.content = self.content.replace(match.group(), self.variables[variable])
+            else:
+                raise ValueError(f"Variable '{variable}' not found in the provided kwargs.")
+            match = re.search(r"{(.*?)}", self.content)
 
-    def translate_if():
-        ...
 
-    def translate_each():
-        ...
+engine = TambusEngine()
+print(engine.translate("<h1>{greeting}</h1>", greeting="Hello"))
