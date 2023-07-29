811872a4118f7d68344a5d6fd656416a0b20dee1
Someone <120342697+Somespi@users.noreply.github.com>
2023-07-29 16:56:06 +0400

    modified:   tambus/src/engine.py

--- tambus/src/engine.py
+++ tambus/src/engine.py
@@ -1,26 +1,28 @@
 import re
 
 class TambusEngine:
-
-    def translate(self, content: str, **kwargs):
+    def translate(self, content: str, kwargs):
         self.variables: dict = kwargs
         self.content: str = content
 
-        if re.search(r"{(.*?)}", self.content):
-            self.translate_variables()
-            
+        if re.search(r"{([^#/:]+?(\[\w+\])?)}", self.content):
+            self.translate_expressions()
+
         return self.content
 
-    def translate_variables(self):
-        match = re.search(r"{(.*?)}", self.content)
+    def translate_expressions(self):
+        match = re.search(r"{([^#/:]+?(\[\w+\])?)}", self.content)
         while match:
-            variable = match.group(1)
-            if variable in self.variables:
-                self.content = self.content.replace(match.group(), self.variables[variable])
-            else:
-                raise ValueError(f"Variable '{variable}' not found in the provided kwargs.")
-            match = re.search(r"{(.*?)}", self.content)
+            variable = match.group()[1:-1]
+            try:
+                self.content = self.content.replace(match.group(), str(eval(match.group()[1:-1], self.variables)))
+                
+            except KeyError:
+                raise ValueError(f"Invalid expression: '{variable}'")
+            except Exception as e:
+                raise ValueError(f"Error evaluating expression: '{variable}'. {e}")
 
+            match = re.search(r"{([^#/:]+?(\[\w+\])?)}", self.content)
 
-engine = TambusEngine()
-print(engine.translate("<h1>{greeting}</h1>", greeting="Hello"))
+t = TambusEngine()
+print(t.translate("<h1>{a}</h1>", {'a': 44}))
