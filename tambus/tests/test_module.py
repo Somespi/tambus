b36fdf24748e3f4a1c8320c551d0320ca0308617
Someone <120342697+Somespi@users.noreply.github.com>
2023-07-30 00:25:58 +0400

    modified:   .gitignore 	modified:   tambus/tests/templates/index.html 	modified:   tambus/tests/test_module.py

--- tambus/tests/test_module.py
+++ tambus/tests/test_module.py
@@ -5,4 +5,4 @@ t = tambus.TambusEngine()
 
 # rendering HTML
 with open("./templates/index.html") as f:
-    print(t.translate(f.read(), hello="world!"))
+    print(t.translate(f.read(), hello=345))
