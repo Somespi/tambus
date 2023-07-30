85b8bb3ced4b9c0a9adc8ef5635151fa09fe28df
Someone <120342697+Somespi@users.noreply.github.com>
2023-07-30 22:10:12 +0400

    update markdown.py

--- tambus/markdown.py
+++ tambus/markdown.py
@@ -5,9 +5,8 @@ class MDEngine:
     def __init__(self, components: list[Component]):
         self.components = components 
     
+    
     def translate(self, content: str):
-        self.content = content 
-        
         for component in self.components:
-            self.content = component.replace_regex(self.content)
-        return self.content
+            content = component.replace_regex(content)
+        return content
